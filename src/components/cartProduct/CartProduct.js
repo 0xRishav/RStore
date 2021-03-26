bbe8c88d0fed58d49c7470ddaf3b2dbbf1524b00
Rishav Bharti <rshvbharti@gmail.com>
2021-03-26 19:08:04 +0530

    style: changed cart product UI

---
+++ src/components/cartProduct/CartProduct.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./CartProduct.css";
+
+function CartProduct({
+  id,
+  name,
+  image,
+  price,
+  description,
+  isInCart,
+  isInWishlist,
+  dispatch,
+  quantity,
+}) {
+  return (
+    <div className="cartProduct">
+      <div className="cartProduct__wrapper">
+        <img className="cartProduct__image" src={image} alt={`${name}image`} />
+        <div className="cartProduct__name">{name}</div>
+        <div className="cartProduct__quantityWrapper">
+          <button
+            className="Product__button--secondary"
+            onClick={() => dispatch({ type: "INCREASE_QUANTITY", payload: id })}
+          >
+            +
+          </button>
+          {quantity}
+          <button
+            className="Product__button--secondary"
+            onClick={() =>
+              dispatch({ type: "DECREASE_QUANTITY", payload: id })
+            }
+          >
+            -
+          </button>
+        </div>
+        <div className="cartProduct__subTotal">Rs. {price * quantity}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CartProduct;
