bedcdbef21ad2c6894895f6a1d956963818538a0
Rishav Bharti <rshvbharti@gmail.com>
2021-03-28 05:56:50 +0530

    made sort radio btns a diffrent component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Product } from "./product/Product";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as CartProduct } from "./cartProduct/CartProduct";
 export { default as Checkbox } from "./checkbox/Checkbox";
+export { default as SortRadioBtns } from "./sort-radio-btns/SortRadioBtns";
