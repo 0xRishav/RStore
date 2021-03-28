6a669980c9cd14d55bedb142fd1624c076192fe7
Rishav Bharti <rshvbharti@gmail.com>
2021-03-28 05:57:56 +0530

    removed sort btn code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Loader, Navbar, Product, CartProduct, Checkbox } from "../index";
+import {
+  Loader,
+  Navbar,
+  Product,
+  CartProduct,
+  Checkbox,
+  SortRadioBtns,
+} from "../index";
 import { ProductsContext } from "../../contexts/productsContext";
 import "./App.css";
 
@@ -33,41 +40,7 @@ function App() {
     <div className="App__wrapper">
       <Navbar />
       <div className="App">
-        <div className="radio-btn-wrapper">
-          <h5>Sort By Price: </h5>
-
-          <label className="radio-btn-container" for="low-to-high">
-            <input
-              type="radio"
-              id="low-to-high"
-              value="sort"
-              name="sort"
-              onChange={() => {
-                dispatch({ type: "SORT_BY", payload: "LOW_TO_HIGH" });
-              }}
-              checked={sortBy && sortBy === "LOW_TO_HIGH"}
-              className="radio-btn"
-            />
-            <span class="checkmark"></span>
-            Low To High
-          </label>
-
-          <label for="high-to-low" className="radio-btn-container">
-            <input
-              type="radio"
-              id="high-to-low"
-              value="sort"
-              name="sort"
-              onChange={() => {
-                dispatch({ type: "SORT_BY", payload: "HIGH_TO_LOW" });
-              }}
-              checked={sortBy && sortBy === "HIGH_TO_LOW"}
-              className="radio-btn"
-            />
-            <span class="checkmark"></span>
-            High To Low
-          </label>
-        </div>
+        {productsToShow === "All Products" && <SortRadioBtns />}
         {isLoading && <Loader />}
         <div className="products-wrapper">
           {productsToShow === "All Products" &&
